c5510772ff7ca044c26be97a2e50d098c09b9905
Mits16 <mits1388@gmail.com>
2024-10-24 18:34:01 +0300

    add test to validate login form with empty date

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -1,4 +1,21 @@
 package gov.login.secure;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://secure.login.gov/");
+        String singInXpath = "//*[@id=\"main-content\"]/div/nav/ul/li[1]/a";
+        By singInBy = By.xpath(singInXpath);
+        WebElement singInElement = driver.findElement(singInBy);
+        singInElement.click();
+    }
+
 }
