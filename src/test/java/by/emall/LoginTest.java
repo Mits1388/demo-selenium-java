28dc71385736f859319277f28e1172a3e17b1ad3
Mits16 <mits1388@gmail.com>
2024-10-27 17:43:25 +0300

    add tests

--- src/test/java/by/emall/LoginTest.java
+++ src/test/java/by/emall/LoginTest.java
@@ -1,6 +1,5 @@
 package by.emall;
 
-import com.netflix.LoginPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -9,35 +8,43 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginTest {
 
     @Test
-    public void testEmptyFields(){
-
+    public void testEmptyFields() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPageMall loginPage = new LoginPageMall(driver);
+        loginPage.clickButton();
+        Assertions.assertEquals("Поле \"Номер телефона\" обязательно для заполнения.\n" +
+                "Поле \"Пароль\" обязательно для заполнения.", loginPage.textEmptyPhone());
     }
 
     @Test
     public void testOnlyPhone() {
-
-    }
-
-    @Test
-    public void testOnlyPassword(){
-
-    }
-
-    @Test
-    public void testUnregisteredUser(){
-
-    }
-
-
-    @Test
-    public void test1() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.netflix.com/login");
-        com.netflix.LoginPage loginPage = new LoginPage(driver);
+        driver.get("https://emall.by/login/password");
+        LoginPageMall loginPage = new LoginPageMall(driver);
+        loginPage.inputPhone();
         loginPage.clickButton();
-        Assertions.assertEquals("Укажите действительный адрес электронной почты или номер телефона.", loginPage.textEmptyEmail());
+        Assertions.assertEquals("Поле \"Пароль\" обязательно для заполнения.", loginPage.textEmptyPassword());
     }
 
+    @Test
+    public void testOnlyPassword() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPageMall loginPage = new LoginPageMall(driver);
+        loginPage.inputPassword();
+        loginPage.clickButton();
+        Assertions.assertEquals("Поле \"Номер телефона\" обязательно для заполнения.", loginPage.textEmptyPhone());
+    }
 
-
+    @Test
+    public void testUnregisteredUser() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPageMall loginPage = new LoginPageMall(driver);
+        loginPage.inputPhone();
+        loginPage.inputPassword();
+        loginPage.clickButton();
+        Assertions.assertEquals("Неверный логин или пароль", loginPage.textInvalidMessage());
+    }
 }
