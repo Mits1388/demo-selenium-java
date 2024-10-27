6468c5d3ddc91e842c2224da2ff71d7d66294858
Mits16 <mits1388@gmail.com>
2024-10-27 17:15:55 +0300

    add class login message

---
+++ src/test/java/by/emall/LoginMessage.java
@@ -0,0 +1,7 @@
+package by.emall;
+
+public class LoginMessage {
+    public static final String EMPTY_PHONE_XPATH ="/html/body/div[1]/div/div/div[1]/form/div[3]";
+    public static final String EMPTY_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[1]/form/div[3]";
+    public static final String INVALID_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[1]/form/div[3]";
+}
